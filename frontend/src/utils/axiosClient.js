c4f5ae5ed351199961d2ade451e8ac7e397a1e24
jasmeet singh <asujasu71@gmail.com>
2026-01-21 14:04:41 +0530

    changes

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,12 +1,28 @@
-import axios from "axios"
+// import axios from "axios"
 
-const axiosClient =  axios.create({
-    baseURL: 'http://localhost:7001',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+// const axiosClient =  axios.create({
+//     baseURL: 'http://localhost:7001',
+//     withCredentials: true,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// });
+
+
+// export default axiosClient;
+
+
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-
 export default axiosClient;
+
+
+console.log(import.meta.env.VITE_API_URL);
